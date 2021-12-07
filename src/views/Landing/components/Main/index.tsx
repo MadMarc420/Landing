106ba0069613d291a00fdea7817440597ed3c023
krypto-dev <kryptodev7@gmail.com>
2021-12-07 18:19:58 +1000

    update UI

--- src/views/Landing/components/Main/index.tsx
+++ src/views/Landing/components/Main/index.tsx
@@ -18,14 +18,14 @@ function Main() {
                 <p>and earn compounding interest</p>
             </div> */}
             <div className="landing-main-btns-wrap">
-                <Link href="https://app.traverse.money" target="_blank" rel="noreferrer">
+                {/* <Link href="https://app.traverse.money" target="_blank" rel="noreferrer">
                     <div className="landing-main-btn">
                         <p>Enter App</p>
                     </div>
-                </Link>
+                </Link> */}
                 <Link href="https://traverse.gitbook.io/traverse/" target="_blank" rel="noreferrer">
                     <div className="landing-main-btn">
-                        <p>More</p>
+                        <p>Documentation</p>
                     </div>
                 </Link>
             </div>
